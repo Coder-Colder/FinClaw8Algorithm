139b58046e7d9eddbff89a237f3df909f71298f6
Coder-Colder <906254852@qq.com>
2020-10-22 14:20:55 +0800

    first version

---
+++ docker_deploy.sh
@@ -0,0 +1,533 @@
+# Copyright 2019-2020 VMware, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# you may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+WORKINGDIR=$(pwd)
+password=0
+datapath=0
+table_name=0
+
+# fetch fate-python image
+source ${WORKINGDIR}/.env
+source ${WORKINGDIR}/parties.conf
+
+cd ${WORKINGDIR}
+
+Deploy() {
+	if [ "$1" = "" ]; then
+		echo "No party id was provided, please check your arguments "
+		exit 1
+	fi
+
+	while [ "$1" != "" ]; do
+		case $1 in
+		splitting_proxy)
+			shift
+			DeployPartyInternal $@
+			break
+			;;
+		all)
+			iii=0
+			for party in ${partylist[*]}; do
+				password=${passwords[iii]}
+				datapath=${datapaths[iii]}
+				table_name=${table_names[iii]}
+				((iii++))
+				if [ "$2" != "" ]; then
+					case $2 in
+					--training)
+						DeployPartyInternal $party
+						if [ "${exchangeip}" != "" ]; then
+							DeployPartyInternal exchange
+						fi
+						;;
+					--serving)
+						DeployPartyServing $party
+						;;
+					esac
+				else
+					DeployPartyInternal $party
+					DeployPartyServing $party
+					if [ "${exchangeip}" != "" ]; then
+						DeployPartyInternal exchange
+					fi
+				fi
+			done
+			break
+			;;
+		*)
+			if [ "$2" != "" ]; then
+				case $2 in
+				--training)
+					DeployPartyInternal $1
+					break
+					;;
+				--serving)
+					DeployPartyServing $1
+					break
+					;;
+				esac
+			else
+				DeployPartyInternal $1
+				DeployPartyServing $1
+			fi
+			;;
+		esac
+		shift
+
+	done
+}
+
+Delete() {
+	if [ "$1" = "" ]; then
+		echo "No party id was provided, please check your arguments "
+		exit 1
+	fi
+
+	while [ "$1" != "" ]; do
+		case $1 in
+		all)
+			iii=0
+			for party in ${partylist[*]}; do
+				password=${passwords[iii]}
+				datapath=${datapaths[iii]}
+				table_name=${table_names[iii]}
+				((iii++))
+				if [ "$2" != "" ]; then
+					DeleteCluster $party $2
+				else
+					DeleteCluster $party
+				fi
+			done
+			if [ "${exchangeip}" != "" ]; then
+				DeleteCluster exchange
+			fi
+			break
+			;;
+		*)
+			DeleteCluster $@
+			break
+			;;
+		esac
+	done
+}
+
+DeployPartyInternal() {
+	target_party_id=$1
+	# should not use localhost at any case
+	target_party_ip="127.0.0.1"
+
+	# check configuration files
+	if [ ! -d ${WORKINGDIR}/outputs ]; then
+		echo "Unable to find outputs dir, please generate config files first."
+		exit 1
+	fi
+	if [ ! -f ${WORKINGDIR}/outputs/confs-${target_party_id}.tar ]; then
+		echo "Unable to find deployment file for party $target_party_id, please generate it first."
+		exit 1
+	fi
+	# extract the ip address of the target party
+	if [ "$target_party_id" = "exchange" ]; then
+		target_party_ip=${exchangeip}
+	elif [ "$2" != "" ]; then
+		target_party_ip="$2"
+	else
+		for ((i = 0; i < ${#partylist[*]}; i++)); do
+			if [ "${partylist[$i]}" = "$target_party_id" ]; then
+				target_party_ip=${partyiplist[$i]}
+			fi
+		done
+	fi
+	# verify the target_party_ip
+	if [ "$target_party_ip" = "127.0.0.1" ]; then
+		echo "Unable to find Party: $target_party_id, please check you input."
+		exit 1
+	fi
+
+	if [ "$3" != "" ]; then
+		user=$3
+	fi
+
+	#scp ${WORKINGDIR}/outputs/confs-$target_party_id.tar $user@$target_party_ip:~/
+	#rm -f ${WORKINGDIR}/outputs/confs-$target_party_id.tar
+	#echo "$target_party_ip training cluster copy is ok!"
+	#ssh -tt $user@$target_party_ip <<eeooff
+
+/usr/bin/expect <<EOF
+	spawn scp ${WORKINGDIR}/outputs/confs-$target_party_id.tar $user@$target_party_ip:~/
+	expect {
+		"(yes/no)?" {
+			send "yes\n"
+			expect "password:"
+			send "$password\n"
+		}
+		"password:" {
+			send "$password\n"
+		}
+	}
+	expect eof
+EOF
+
+/usr/bin/expect <<EOF
+	spawn scp ${WORKINGDIR}/script.py $user@$target_party_ip:~/
+	expect {
+		"(yes/no)?" {
+			send "yes\n"
+			expect "password:"
+			send "$password\n"
+		}
+		"password:" {
+			send "$password\n"
+		}
+	}
+	expect eof
+EOF
+
+	echo "$target_party_ip training cluster copy is ok!"
+
+/usr/bin/expect <<EOF
+	spawn ssh $user@$target_party_ip
+	expect {
+		"(yes/no)?" {
+			send "yes\n"
+			expect "password:"
+			send "$password\n"
+		}
+		"password:" {
+			send "$password\n"
+		}
+	}
+	expect "#"
+	send "ls\r"
+	expect "#"
+	send "mkdir -p $dir\r"
+	expect "#"
+	send "rm -f $dir/confs-$target_party_id.tar\r"
+	expect "#"
+	send "mv ~/confs-$target_party_id.tar $dir\r"
+	expect "#"
+	send "cd $dir\r"
+	expect "#"
+	send "tar -xzf confs-$target_party_id.tar\r"
+	expect "#"
+	send "cd confs-$target_party_id\r"
+	expect "#"
+	send "docker-compose down\r"
+	expect "#"
+	send "docker volume rm -f confs-${target_party_id}_shared_dir_examples\r"
+	expect "#"
+	send "docker volume rm -f confs-${target_party_id}_shared_dir_federatedml\r"
+	expect "#"
+	send "docker-compose up -d\r"
+	expect "#"
+	send "rm -f ../confs-${target_party_id}.tar\r"
+	expect "#"
+	send "docker-compose up -d\r"
+    expect "#"
+	send "exit\r"
+	expect eof
+EOF
+
+	echo "party ${target_party_id} deploy is ok!"
+}
+
+DeployPartyServing() {
+	target_party_id=$1
+	# should not use localhost at any case
+	target_party_serving_ip="127.0.0.1"
+
+	# check configuration files
+	if [ ! -d ${WORKINGDIR}/outputs ]; then
+		echo "Unable to find outputs dir, please generate config files first."
+		exit 1
+	fi
+	if [ ! -f ${WORKINGDIR}/outputs/serving-${target_party_id}.tar ]; then
+		echo "Unable to find deployment file for party $target_party_id, please generate it first."
+		exit 1
+	fi
+	# extract the ip address of the target party
+	for ((i = 0; i < ${#partylist[*]}; i++)); do
+		if [ "${partylist[$i]}" = "$target_party_id" ]; then
+			target_party_serving_ip=${servingiplist[$i]}
+		fi
+	done
+	# verify the target_party_ip
+	if [ "$target_party_serving_ip" = "127.0.0.1" ]; then
+		echo "Unable to find Party : $target_party_id serving address, please check you input."
+		exit 1
+	fi
+
+	#scp ${WORKINGDIR}/outputs/serving-$target_party_id.tar $user@$target_party_serving_ip:~/
+	#echo "party $target_party_id serving cluster copy is ok!"
+	#ssh -tt $user@$target_party_serving_ip <<eeooff
+/usr/bin/expect <<EOF
+	spawn scp ${WORKINGDIR}/outputs/serving-$target_party_id.tar $user@$target_party_serving_ip:~/
+	expect {
+		"(yes/no)?" {
+			send "yes\n"
+			expect "password:"
+			send "$password\n"
+		}
+		"password:" {
+			send "$password\n"
+		}
+	}
+	expect eof
+EOF
+	echo "party $target_party_id serving cluster copy is ok!"
+
+/usr/bin/expect <<EOF
+	spawn ssh $user@$target_party_serving_ip
+	expect {
+		"(yes/no)?" {
+			send "yes\n"
+			expect "password:"
+			send "$password\n"
+		}
+		"password:" {
+			send "$password\n"
+		}
+	}
+	expect "#"
+	send "ls\r"
+	expect "#"
+	send "mkdir -p $dir\r"
+	expect "#"
+	send "rm -f $dir/serving-$target_party_id.tar\r"
+	expect "#"
+	send "mv ~/serving-$target_party_id.tar $dir\r"
+	expect "#"
+	send "cd $dir\r"
+	expect "#"
+	send "tar -xzf serving-$target_party_id.tar\r"
+	expect "#"
+	send "cd serving-$target_party_id\r"
+	expect "#"
+	send "docker-compose down\r"
+	expect "#"
+	send "docker-compose up -d\r"
+	expect "#"
+	send "rm -f ../serving-$target_party_id.tar\r"
+	expect "#"
+	send "docker-compose up -d\r"
+	expect "#"
+    send "exit\r"
+EOF
+	echo "party $target_party_id serving cluster deploy is ok!"
+
+/usr/bin/expect<<EOF
+    spawn ssh $user@$target_party_serving_ip
+	expect {
+		"(yes/no)?" {
+			send "yes\n"
+			expect "password:"
+			send "$password\n"
+		}
+		"password:" {
+			send "$password\n"
+		}
+	}
+	expect "#"
+	send "docker ps\r"
+    expect "#"
+    send "docker exec -it confs-${target_party_id}_python_1 bash\r"
+    expect "#"
+    send "mkdir -p $project\r"
+    expect "#"
+    send "exit\r"
+	expect "#"
+	send "docker cp $datapath confs-${target_party_id}_python_1:/data/projects/fate/python/${project}/data.csv\r"
+	expect "#"
+	send "docker cp ~/script.py confs-${target_party_id}_python_1:/data/projects/fate/python/${project}/\r"
+    expect "#"
+    send "docker exec -it confs-${target_party_id}_python_1 bash\r"
+    expect "#"
+    send "cd ${project}\r"
+    expect "#"
+    send "python3 script.py -f upload -tb $table_name -proj $project -dp /data/projects/fate/python/${project}/data.csv\r"
+    expect "#"
+    send "exit\r"
+	expect "#"
+	send "rm ~/script.py\r"
+	expect "#"
+EOF
+    echo "party $target_party_id upload dataset is ok!"
+}
+
+DeleteCluster() {
+	target_party_id=$1
+	cluster_type=$2
+	target_party_serving_ip="127.0.0.1"
+	target_party_ip="127.0.0.1"
+
+	# extract the ip address of the target party
+	if [ "$target_party_id" == "exchange" ]; then
+		target_party_ip=${exchangeip}
+	else
+		for ((i = 0; i < ${#partylist[*]}; i++)); do
+			if [ "${partylist[$i]}" = "$target_party_id" ]; then
+				target_party_ip=${partyiplist[$i]}
+			fi
+		done
+	fi
+
+	for ((i = 0; i < ${#partylist[*]}; i++)); do
+		if [ "${partylist[$i]}" = "$target_party_id" ]; then
+			target_party_serving_ip=${servingiplist[$i]}
+		fi
+	done
+
+	# delete training cluster
+	if [ "$cluster_type" == "--training" ]; then
+/usr/bin/expect <<EOF
+		spawn ssh $user@$target_party_ip
+		expect {
+			"(yes/no)?" {
+				send "yes\n"
+				expect "password:"
+				send "$password\n"
+			}
+			"password:" {
+				send "$password\n"
+			}
+		} 
+		expect "#"
+		send "cd $dir/confs-$target_party_id\r"
+		expect "#"
+		send "docker-compose down\r"
+		expect "#"
+		send "exit\r"
+		expect eof
+EOF
+		#ssh -tt $user@$target_party_ip 
+		echo "party $target_party_id training cluster is deleted!"
+	# delete serving cluster
+	elif [ "$cluster_type" == "--serving" ]; then
+/usr/bin/expect <<EOF
+		spawn ssh $user@$target_party_serving_ip
+		expect {
+			"(yes/no)?" {
+				send "yes\n"
+				expect "password:"
+				send "$password\n"
+			}
+			"password:" {
+				send "$password\n"
+			}
+		}
+		expect "#"
+		send "cd $dir/serving-$target_party_id\r"
+		expect "#"
+		send "docker-compose down\r"
+		expect "#"
+		send "exit\r"
+		expect eof
+EOF
+		#ssh -tt $user@$target_party_serving_ip 
+		echo "party $target_party_id serving cluster is deleted!"
+	# delete training cluster and serving cluster
+	else
+		# if party is exchange then delete exchange cluster
+		if [ "$target_party_id" == "exchange" ]; then
+/usr/bin/expect <<EOF
+		spawn ssh $user@$target_party_ip
+		expect {
+			"(yes/no)?" {
+				send "yes\n"
+				expect "password:"
+				send "$password\n"
+			}
+			"password:" {
+				send "$password\n"
+			}
+		}
+		expect "#"
+		send "cd $dir/confs-$target_party_id\r"
+		expect "#"
+		send "docker-compose down\r"
+		expect "#"
+		send "exit\r"
+		expect eof
+EOF
+			#ssh -tt $user@$target_party_ip 
+		else
+/usr/bin/expect <<EOF
+		spawn ssh $user@$target_party_ip
+		expect {
+			"(yes/no)?" {
+				send "yes\n"
+				expect "password:"
+				send "$password\n"
+			}
+			"password:" {
+				send "$password\n"
+			}
+		}
+		expect "#"
+		send "cd $dir/confs-$target_party_id\r"
+		expect "#"
+		send "docker-compose down\r"
+		expect "#"
+		send "exit\r"
+		expect eof
+EOF
+			#ssh -tt $user@$target_party_ip 
+			echo "party $target_party_id training cluster is deleted!"
+/usr/bin/expect <<EOF
+			spawn ssh $user@$target_party_serving_ip
+			expect {
+				"(yes/no)?" {
+					send "yes\n"
+					expect "password:"
+					send "$password\n"
+				}
+				"password:" {
+					send "$password\n"
+				}
+			}
+			expect "#"
+			send "cd $dir/serving-$target_party_id\r"
+			expect "#"
+			send "docker-compose down\r"
+			expect "#"
+			send "exit\r"
+			expect eof
+EOF
+			#ssh -tt $user@$target_party_serving_ip 
+			echo "party $target_party_id serving cluster is deleted!"
+		fi
+	fi
+}
+
+ShowUsage() {
+	echo "Usage: "
+	echo "Deploy all parties or specified partie(s): bash docker_deploy.sh partyid1[partyid2...] | all"
+}
+
+main() {
+	if [ "$1" = "" ] || [ "$" = "--help" ]; then
+		ShowUsage
+		exit 1
+	elif [ "$1" = "--delete" ] || [ "$1" = "--del" ]; then
+		shift
+		Delete $@
+	else
+		Deploy "$@"
+	fi
+
+	exit 0
+}
+
+main $@
---
+++ script.py
@@ -0,0 +1,147 @@
+# Utils
+import json
+import os
+import logging
+
+UPLOAD_JSON_PATH = os.path.join(os.curdir, 'upload_data.json')
+PARTIES_PATH = "./parties.conf"
+DATA_PATH = os.path.join(os.curdir, 'train_data.csv')
+
+fate_flow_path = "../fate_flow/fate_flow_client.py"
+#dsl_path = os.path.join(home_dir, "toy_example_dsl.json")
+#conf_path = os.path.join(home_dir, "toy_example_conf.json")
+
+
+def generateTableName(proj_name, cnt):
+    list = []
+    for i in range(cnt):
+        list.append(proj_name + "_" + str(i))
+    return list
+
+
+def create_parties_json(proj_name, ip_list, party_list, passwords, datapaths, usernames=[]):
+    with open(PARTIES_PATH, 'w+') as f:
+        f.write('#!/bin/bash\n\n')
+        f.write("project={}\n".format(proj_name))
+        f.write("user=root\n")
+        f.write("dir=/data/projects/fate\n")
+        f.write("users=({})\n".format(" ".join(usernames)))
+        f.write("datapaths=({})\n".format(" ".join(datapaths)))
+        f.write("passwords=({})\n".format(" ".join(passwords)))
+        f.write("partylist=({})\n".format(" ".join(party_list)))
+        f.write("partyiplist=({})\n".format(" ".join(ip_list)))
+        f.write("servingiplist=({})\n".format(" ".join(ip_list)))
+        f.write("table_names=({})\n".format(" ".join(generateTableName(proj_name, len(ip_list)))))
+        f.write("#exchangeip=\n")
+        f.write('''# modify if you are going to use an external db
+mysql_ip=mysql
+mysql_user=fate
+mysql_password=fate_dev
+mysql_db=fate_flow
+
+# modify if you are going to use an external redis
+redis_ip=redis
+redis_port=6379
+redis_password=fate_dev''')
+    f.close()
+
+
+# 创建上传数据文件
+def create_upload_json(data_path, task_name, table_name: str):
+    upload_dict = {}
+    upload_dict["head"] = 1
+    upload_dict["file"] = data_path
+    upload_dict["partition"] = 10
+    upload_dict["work_mode"] = 1
+    upload_dict["namespace"] = task_name
+    upload_dict["table_name"] = table_name
+    with open(UPLOAD_JSON_PATH, 'w') as f:
+        json.dump(upload_dict, f, sort_keys=True, indent=4, separators=(', ', ':'))
+    f.close()
+
+
+# Argument Parser
+import argparse
+import os
+
+parser = argparse.ArgumentParser(add_help=True, description="Fedarated Learning command parser")
+parser.add_argument("-f", "--function", type=str, choices=["deploy", "upload", "submit", "delete"], required=True,
+                    help="choose the function to execute.")
+
+parser.add_argument("-u", "--users", type=str, nargs='*',
+                    help="username for logging in destination host, default--root.The first username will be regarded \
+as the user on regulator host. Need to specify when using '-f deploy'.")
+
+parser.add_argument("-pw", "--password", type=str, nargs='+',
+                    help="password for each user respectively.The first password will be regarded \
+as the password for logging in regulator host. Need to specify when using '-f deploy'.")
+
+parser.add_argument("-id", "--id", type=str, nargs='+',
+                    help="party id assigned to each host respectively.The first id will be regarded \
+as the id assigned to regulator host. Need to specify when using '-f deploy'.")
+
+parser.add_argument("-ip", "--ip", type=str, nargs='+',
+                    help="ipv4 address of each host respectively.The first ip will be regarded \
+as the ip of regulator host. Need to specify when using '-f deploy'.")
+
+parser.add_argument("-p", "--path", type=str, nargs='+', metavar='dataPath',
+                    help="path of dataset(.csv file) on each host respectively. The first path will be regarded \
+as path of dataset on regulator host. Need to specify when using '-f deploy'.")
+
+parser.add_argument("-proj", "--project", type=str,
+                    help="name of the training project. Need to specify when using '-f deploy' or '-f upload'.")
+
+parser.add_argument("-dp", "--datapath", type=str,
+                    help="path of dataset to upload on local host. Need to specify when using '-f upload'")
+
+parser.add_argument("-tb", "--tablename", type=str,
+                    help="name of data table to configure upload.json. Need to specify when using '-f upload'")
+
+parser.add_argument("-alg", "--algorithm", type=str, choices=["SecureBoost"],
+                    help="configure the Machine Learning Algorithm.")
+
+args = parser.parse_args()
+
+
+import subprocess
+
+
+def run_cmd(cmd):
+    logging.info(f"cmd: {' '.join(cmd)}")
+    subp = subprocess.Popen(cmd,
+                            shell=False,
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
+    subp.wait()
+    stdout, stderr = subp.communicate()
+    return stdout.decode("utf-8")
+
+
+def deploy():
+    if args.users == None:
+        users = ["root"]
+    else:
+        users = args.users
+    create_parties_json(args.project, args.ip, args.id, args.password, args.path, users)
+    os.system("bash ./generate_config.sh")
+    os.system("bash ./docker_deploy.sh all")
+
+
+def upload():
+    create_upload_json(args.datapath, args.project, args.tablename)
+    os.system(" ".join(["python", fate_flow_path, "-f", "upload", "-c", args.datapath]))
+
+
+def delete():
+    os.system("bash ./docker_deploy.sh --delete all")
+
+def submit():
+    pass
+
+
+if args.function == "deploy":
+    deploy()
+elif args.function == "submit":
+    submit()
+elif args.function == "upload":
+    upload()
