f9aed3bbd74391df6e002763211bd1b0bd808cad
Coder-Colder <906254852@qq.com>
2021-02-08 20:24:37 +0800

    fix bugs

--- script.py
+++ script.py
@@ -13,8 +13,10 @@ fate_flow_path = "../fate_flow/fate_flow_client.py"
 run_task_path = "./run_task_script/run_task.py"
 job_conf_path = "./run_task_script/config/test_hetero_linr_train_job_conf.json"
 job_dsl_path = "./run_task_script/config/test_hetero_linr_train_job_dsl.json"
-#dsl_path = os.path.join(home_dir, "toy_example_dsl.json")
-#conf_path = os.path.join(home_dir, "toy_example_conf.json")
+
+
+# dsl_path = os.path.join(home_dir, "toy_example_dsl.json")
+# conf_path = os.path.join(home_dir, "toy_example_conf.json")
 
 
 def generateTableName(proj_name, cnt):
@@ -23,12 +25,13 @@ def generateTableName(proj_name, cnt):
         list.append(proj_name + "_" + str(i))
     return list
 
+
 def create_upload_conf(party_path, party2ip, party2usr, party2pswd, project):
-    usernames=[]
-    passwords=[]
-    party_list=[]
-    ip_list=[]
-    plist=[]
+    usernames = []
+    passwords = []
+    party_list = []
+    ip_list = []
+    plist = []
     for item in party_path:
         id, path = item
         usernames.append(party2usr[id])
@@ -36,7 +39,7 @@ def create_upload_conf(party_path, party2ip, party2usr, party2pswd, project):
         party_list.append(id)
         ip_list.append(party2ip[id])
         plist.append(path)
-    tlist=generateTableName(project, len(plist))
+    tlist = generateTableName(project, len(plist))
 
     with open(UPLOAD_CONF_PATH, "w+") as f:
         f.write("#!/bin/bash\n\n")
@@ -50,6 +53,7 @@ def create_upload_conf(party_path, party2ip, party2usr, party2pswd, project):
         f.write("table_names=({})\n".format(" ".join(tlist)))
         f.write("datapaths=({})\n".format(" ".join(plist)))
         f.write("project={}\n".format(project))
+
 
 def create_parties_json(ip_list, party_list, passwords, usernames=[]):
     with open(PARTIES_PATH, 'w+') as f:
@@ -131,12 +135,15 @@ def get_guest_ip(data_path):
     f.close()
     return guest_ip
 
+
 # Argument Parser
 import argparse
 import os
 
 parser = argparse.ArgumentParser(add_help=True, description="Fedarated Learning command parser")
-parser.add_argument("-f", "--function", type=str, choices=["deploy", "delete", "upload", "r_upload", "submit", "load_bind", "predict", "query", "r_query"], required=True,
+parser.add_argument("-f", "--function", type=str,
+                    choices=["deploy", "delete", "upload", "r_upload", "submit", "load_bind", "predict", "query",
+                             "r_query"], required=True,
                     help="choose the function to execute.")
 
 parser.add_argument("-u", "--users", type=str, nargs='*',
@@ -197,7 +204,6 @@ parser.add_argument("-v", "--verbose", help="increase output verbosity", action=
 
 args = parser.parse_args()
 
-
 import subprocess
 
 
@@ -209,6 +215,7 @@ def run_cmd(cmd):
                             stderr=subprocess.STDOUT)
     subp.wait()
     stdout, stderr = subp.communicate()
+    print(subp.returncode)
     return stdout.decode("utf-8")
 
 
@@ -225,22 +232,21 @@ def deploy(iplist, idlist, passwordlist, users):
     else:
         run_cmd(["bash", "./generate_config.sh"])
         stdout = run_cmd(["bash", "./docker_deploy.sh", "all"])
-        
+
         '''
         every successful exec will form 2 exits, so test the number of exit 
         '''
+
         def check_deploy_valid(str):
-            pattern = r"exit"
+            pattern = r"(error)|(ERROR)"
             compiled_pattern = re.compile(pattern)
-            success_res = compiled_pattern.findall(str)
-            return len(success_res) == 2*len(iplist)
-        
+            error_res = compiled_pattern.findall(str)
+            return len(error_res) == 0
+
         if check_deploy_valid(stdout):
             print("deploy success.")
         else:
             print("deploy failure.")
-    '''TODO: judge success or not?'''
-
 
 
 def _upload(datapath, project, tablename):
@@ -268,9 +274,9 @@ def getPartyInfo():
                 plist = line.strip("\n").strip("passwords=").strip("(").strip(")").split(" ")
             elif line.find("partylist") != -1:
                 idlist = line.strip("\n").strip("partylist=").strip("(").strip(")").split(" ")
-    party2pswd={}
-    party2usr={}
-    party2ip={}
+    party2pswd = {}
+    party2usr = {}
+    party2ip = {}
     for idx in range(len(idlist)):
         party2ip[idlist[idx]] = iplist[idx]
         party2usr[idlist[idx]] = ulist[idx]
@@ -285,7 +291,7 @@ def upload(guest_pair, host_pair, project):
         host_pair = []
     party_path = [(guest_pair[0], guest_pair[1])]
     for idx in range(0, len(host_pair), 2):
-        party_path.append((host_pair[idx], host_pair[idx+1]))
+        party_path.append((host_pair[idx], host_pair[idx + 1]))
     party2ip, party2usr, party2pswd = getPartyInfo()
     create_upload_conf(party_path, party2ip, party2usr, party2pswd, project)
     if args.verbose:
@@ -297,15 +303,15 @@ def upload(guest_pair, host_pair, project):
             pattern = "'retcode':\s+([0-9]+),"
             re_exp = re.compile(pattern)
             rets = re_exp.findall(ret_val)
-            if len(rets) != (len(guest_pair) + len(host_pair))/2:
+            if len(rets) != (len(guest_pair) + len(host_pair)) / 2:
                 return False
-            
+
             for val in rets:
                 if int(val) != 0:
                     return False
-            
+
             return True
-        
+
         if check_valid_retcode(stdout):
             print("upload success.")
         else:
@@ -314,7 +320,13 @@ def upload(guest_pair, host_pair, project):
     '''TODO: judge success or not?'''
 
 
-def delete():
+def delete(iplist, idlist, passwordlist, users):
+    if users == None:
+        users = ["root"]
+    if len(users) != len(idlist):
+        for i in range(len(idlist) - len(users)):
+            users.append("root")
+    create_parties_json(iplist, idlist, passwordlist, users)
     if args.verbose:
         os.system("bash ./docker_deploy.sh --delete all")
     else:
@@ -329,15 +341,14 @@ def submit(alg, proj, work_mode):
         os.system("bash ./upload.sh --submit -m {} -alg {} -proj {}".format(work_mode, alg, proj))
     else:
         run_cmd("bash ./upload.sh --submit -m {} -alg {} -proj {}".format(work_mode, alg, proj).split(" "))
-    '''TODO: judge success or not?'''
-    info={}
+    info = {}
     info["retcode"] = 0
     with open("./info.txt", "r") as f:
+        f.readline()  # pass the useless params
         info["model_id"] = f.readline().strip("\n")
         info["model_version"] = f.readline().strip("\n")
         info["jobid"] = f.readline().strip("\n")
     print(info)
-
 
 
 def bind(model_name, model_id, model_version):
@@ -361,15 +372,16 @@ def predict(model_name, params):
 
 
 def _query(jobid):
-    ret = eval(run_cmd(["python", fate_flow_path, "-f", "query_job", "-j", jobid, "-r", "guest"]))
+    ret = run_cmd(["python", fate_flow_path, "-f", "query_job", "-j", jobid, "-r", "guest"])
     ret = json.loads(ret)
+    print(ret)
     status = ret["retcode"]
     if status != 0:
         print("running")
     check_data = ret["data"]
     with open("../info.txt", "w+") as f:
-        for i in len(check_data):
-            f.write(check_data[i]["f_state"]+"\n")
+        for i in range(len(check_data)):
+            f.write(check_data[i]["f_state"] + "\n")
 
 def query(jobid):
     if args.verbose:
@@ -379,7 +391,7 @@ def query(jobid):
     ret = ""
     with open("./info.txt", "r") as f:
         for line in f.readlines():
-            ret += line.strip("\n")
+            ret += line
     print(ret)
 
 
@@ -390,7 +402,7 @@ elif args.function == "submit":
 elif args.function == "r_upload":
     _upload(args.datapath, args.project, args.tablename)
 elif args.function == "delete":
-    delete()
+    delete(args.ip, args.id, args.password, args.users)
 elif args.function == "load_bind":
     bind(args.model_name, args.model_id, args.model_version)
 elif args.function == "upload":
